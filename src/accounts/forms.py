997cd5139c38003db1551999f0d38267632346c7
ryopan <r.sugiyamadining@gmail.com>
2026-03-03 22:14:45 +0900

    chore: add accounts app

---
+++ src/accounts/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class SignupForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['login_id']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            self.add_error('password2', 'パスワードが一致しません。')
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    login_id = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
